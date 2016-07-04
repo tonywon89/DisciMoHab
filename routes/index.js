5b6f999ed48181d20f9447b6a8252832c99b9d4f
Tony Won <tonywon89@gmail.com>
2016-07-03 19:26:02 -0700

    Update readme

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
-var path = require('path');
-var connectionString = require(path.join(__dirname, '../', 'config'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
