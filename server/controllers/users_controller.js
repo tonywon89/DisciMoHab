6134fa196b013863ba07b06fa87898beb2912f1d
Tony Won <tonywon89@gmail.com>
2016-07-04 15:30:05 -0700

    Successfully use bcrypt to register a user

--- server/controllers/users_controller.js
+++ server/controllers/users_controller.js
@@ -4,12 +4,11 @@ const saltRounds = 10;
 
 var UsersController = {
   createUser: function (req, res, next) {
-    // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    //   User.registerUser(req.body.username, hash, function () {
-    //     res.redirect("/");
-    //   });
-    // });
-    res.send(`A user has been created ${req.body.username} ${req.body.password}`);
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      User.registerUser(req.body.username, hash, function () {
+        res.send("A user has successfully been created!");
+      });
+    });
   }
 };
 
